5c8df3a063e0d1aa8dacce540424856f2f50d87c
Eugene Karanda <eugene.karanda@gmail.com>
2017-09-29 18:14:10 +0300

    added test 'context load'

---
+++ src/test/java/org/overmind/mfk/server/ServerApplicationTest.java
@@ -0,0 +1,18 @@
+package org.overmind.mfk.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author eugene.karanda
+ * @version 1.0 Create: 29.09.2017 18:10
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerApplicationTest {
+
+    @Test
+    public void contextLoad() throws Exception {}
+}
